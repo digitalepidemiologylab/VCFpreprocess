296d022ea83324d102f47927b7348f503f98b31a
Odissea <theo.dupuis@gmail.com>
2016-09-27 18:54:24 +0200

    Finit pour aujourd'hui !

---
+++ selectsnpsfromfile.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+refcsvfile = "./MatchingReferences.csv"
+poscsvfile = "./MatchingPositions.csv"
+
+
+matchingref = pd.read_csv(refcsvfile, sep= "\t")
+matchingpos = pd.read_csv(poscsvfile, sep = "\t")
+
+
+print(matchingpos.head(10))
+print(matchingref.head(10))
