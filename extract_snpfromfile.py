b6a807054a9f081115452bd6b10c71eff43fe8e1
Odissea <theo.dupuis@gmail.com>
2016-09-28 11:57:43 +0200

    Modifications du code extract_snpfromfile.py pour le rendre un peu plus propre. \nDebut du travail sur la sur partie selectsnpsfromfile.py qui sera à la suite du script sus-mentionné.

--- extract_snpfromfile.py
+++ extract_snpfromfile.py
@@ -3,13 +3,56 @@ import pandas as pd
 import glob
 import os
 import re
+import gzip
 
-#######################Works with python 2.7.12 but not 3.5.2
+#######################/!\Works with python 2.7.12 but not 3.5.2 /!\############################
 
 def natural_sort(l): 
     convert = lambda text: int(text) if text.isdigit() else text.lower() 
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(l, key = alphanum_key)
+
+def list_vcf_files(PATH):
+	vcffiles = []
+	iterator = 0
+	for files in glob.glob('*.vcf.gz'):
+		iterator+=1
+		vcffiles.append(files)
+		print("File found at {0} : {1}".format(PATH,files))
+
+	print("Number of files found in {0} : {1}".format(PATH, iterator))
+
+	####Naturally sort the files so that the chromosomes are processed in the roght order
+	vcffiles = natural_sort(vcffiles)
+	return vcffiles
+
+def find_matches(vcffiles, refsnps) :
+
+	refids = refsnps["SNP"].tolist()
+	refpositions = refsnps["Position"].tolist()	
+	matchingreferences = pd.DataFrame()
+	matchingpositions = pd. DataFrame()
+
+	for file in vcffiles:
+
+		print("Loading file {} in a dataframe ...".format(file))
+		chromosomefile = VCF.dataframe(file).drop(["ALT", "REF", "QUAL", "FILTER", "INFO"], 1)
+		###Number of lines to shift to make dataframe index correspond with lines in actual vcf file (comments + 1 (dataframe index starts at 0)) 
+		skip = VCF._count_comments(file)+1
+		chromosomefile["Corresponding row in vcf file"] = range(skip,chromosomefile.shape[0]+skip)
+		
+		print("Searching for matches with the reference dataframes...")
+		newmatchingreferences = pd.concat((matchingreferences, chromosomefile[chromosomefile['ID'].isin(refids)]))
+		newmatchingpositions = pd.concat((matchingpositions, chromosomefile[chromosomefile["POS"].isin(refpositions)]))
+
+		print("{0} ref matching and {1} positions matching in {2}".format(newmatchingreferences.shape[0] - matchingreferences.shape[0], newmatchingpositions.shape[0] - matchingpositions.shape[0], file))
+
+		matchingreferences = newmatchingreferences
+		matchingpositions = newmatchingpositions
+	print("We found {} matching ref of snps.".format(matchingreferences.shape[0]))
+
+	print("We found {} matching positions of snps.".format(matchingpositions.shape[0]))
+	return matchingreferences, matchingpositions
 
 
 ###################################################################
@@ -18,48 +61,28 @@ REFSNPFILE= "./snpslinkedwithheight.csv"
 IT = 0
 NBCORPOS = 0
 NBCORID = 0
-vcffiles = []
-matchingreferences = pd.DataFrame()
-matchingpositions = pd. DataFrame()
+
 
 
 ####Load list of snp in a dataframe (column 1 = snp ref name, column 2 = chr, column 3 = position)
 ####Also convert snpref to strings 
 refsnps = pd.read_csv(REFSNPFILE, sep = ",")
-refids = refsnps["SNP"].tolist()
-refpositions = refsnps["Position"].tolist()
+
 
 ####Looking at the files present in the directory before working on them
 
-for files in glob.glob('*.vcf.gz'):
-	IT+=1
-	vcffiles.append(files)
-	print("File found at {0} : {1}".format(PATH,files))
+vcffiles = list_vcf_files(PATH)
 
-print("Number of files found in {0} : {1}".format(PATH, IT))
+#### Find the reference SNPs in the actual vcf files
+#matchingreferences, matchingpositions = find_matches(vcffiles, refsnps)
 
-###Naturally sort the files so that the chromosomes are processed in the roght order
-vcffiles = natural_sort(vcffiles)
 
-####
-for file in vcffiles:
+####Save the list of SNPs found
+#matchingpositions.to_csv(path_or_buf="./MatchingPositions.csv", sep="\t", index=False)
+#matchingreferences.to_csv(path_or_buf="./MatchingReferences.csv", sep="\t", index=False)
 
-	print("Loading file {} in a dataframe ...".format(file))
-	chromosomefile = VCF.dataframe(file).drop(["ALT", "REF", "QUAL", "FILTER", "INFO"], 1)
-	skip = VCF._count_comments(file)+2
-	chromosomefile["Row"] = range(skip,chromosomefile.shape[0]+skip)
-	
-	print("Searching for matches with the reference dataframes...")
-	newmatchingreferences = pd.concat((matchingreferences, chromosomefile[chromosomefile['ID'].isin(refids)]))
-	newmatchingpositions = pd.concat((matchingpositions, chromosomefile[chromosomefile["POS"].isin(refpositions)]))
 
-	print("{0} ref matching and {1} positions matching in {2}".format(newmatchingreferences.shape[0] - matchingreferences.shape[0], newmatchingpositions.shape[0] - matchingpositions.shape[0], file))
 
-	matchingreferences = newmatchingreferences
-	matchingpositions = newmatchingpositions
-print("We found {} matching ref of snps.".format(matchingreferences.shape[0]))
+if not os.path.isdir("./SelectionofSNPs") :
+	os.mkdir("./SelectionofSNPs")
 
-print("We found {} matching positions of snps.".format(matchingpositions.shape[0]))
-
-matchingpositions.to_csv(path_or_buf="./MatchingPositions.csv", sep="\t", index=False)
-matchingreferences.to_csv(path_or_buf="./MatchingReferences.csv", sep="\t", index=False)
--- selectsnpsfromfile.py
+++ selectsnpsfromfile.py
@@ -1,14 +1,51 @@
 import pandas as pd
 import os
 import glob
+import re
 
+#####VARIABLES TO DECLARE AT THE BEGINNING OF THE COMPLETE SCRIPT
+###############################################
+####Strings to remove to extract chromosome number
+BEFORECHRNB = ""
+AFTERCHRNB = ".QC.vcf.gz.vcf.gz"
+###############################################
+
+
+####TO BE REMOVED WHEN MERGED WITH THE REST OF THE SCRIPT
+###############################################
 refcsvfile = "./MatchingReferences.csv"
 poscsvfile = "./MatchingPositions.csv"
 
+matchingreferences = pd.read_csv(refcsvfile, sep= "\t")
+matchingpositions = pd.read_csv(poscsvfile, sep = "\t")
 
-matchingref = pd.read_csv(refcsvfile, sep= "\t")
-matchingpos = pd.read_csv(poscsvfile, sep = "\t")
+print(matchingpositions.head(5))
+print(matchingreferences.head(5))
+PATH = "./"
+import extract_snpfromfile
+vcffiles = extract_snpfromfile.list_vcf_files(PATH)
+###############################################
+
+for files in vcffiles :
+
+	if not os.path.isdir(PATH+"/SelectionofSNPs") :
+		os.mkdir(PATH+"/SelectionofSNPs")
 
 
-print(matchingpos.head(10))
-print(matchingref.head(10))
+	#####Copy header and column labels in a subset file (<chrnb>.subset.vcf) in PATH/SelectionofSNPs
+
+
+ 	#####Filter dataframe to have only snps corresponding to the file
+	chrnb = int(re.sub(BEFORECHRNB, '', re.sub(AFTERCHRNB, '', files)))
+
+
+	####Loop through the indexes of the filtered df
+
+		####Check that pos and ref are ok
+
+			####Add line to subset file 
+
+
+
+	####compress the output file to .gz
+ 	
