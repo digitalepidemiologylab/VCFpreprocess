42fee605fcaba3d60dfdd087dc9f993cf37123ad
Odissea <theo.dupuis@gmail.com>
2016-09-28 16:00:59 +0200

    Sauvegarde de milieu de journée. ça avance mais je vais devoir utiliser du bash ...

--- selectsnpsfromfile.py
+++ selectsnpsfromfile.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python2.7.12
+# -*-coding:Utf-8 -*
 import pandas as pd
 import os
 import glob
 import re
+import gzip
 
 #####VARIABLES TO DECLARE AT THE BEGINNING OF THE COMPLETE SCRIPT
 ###############################################
@@ -26,17 +29,47 @@ import extract_snpfromfile
 vcffiles = extract_snpfromfile.list_vcf_files(PATH)
 ###############################################
 
+
+
 for files in vcffiles :
+
+	chrnb = int(re.sub(BEFORECHRNB, '', re.sub(AFTERCHRNB, '', files)))
+
 
 	if not os.path.isdir(PATH+"/SelectionofSNPs") :
 		os.mkdir(PATH+"/SelectionofSNPs")
+	if not os.path.isdir(PATH+"/SelectionofSNPs/ID") :
+		os.mkdir(PATH+"/SelectionofSNPs/ID")
+	if not os.path.isdir(PATH+"/SelectionofSNPs/POS") :
+		os.mkdir(PATH+"/SelectionofSNPs/POS")
+	if not os.path.isdir(PATH+"/SelectionofSNPs/HEADER") :
+		os.mkdir(PATH+"/SelectionofSNPs/HEADER")
+
+	outputfileID = PATH+"/SelectionofSNPs/ID/"+str(chrnb)+".subset_ID.vcf"
+	outputfilePOS = PATH+"/SelectionofSNPs/POS/"+str(chrnb)+".subset_POS.vcf"
+	outputfileHEADER = PATH+"/SelectionofSNPs/HEADER/"+str(chrnb)+".subset_HEADER.vcf"
 
 
-	#####Copy header and column labels in a subset file (<chrnb>.subset.vcf) in PATH/SelectionofSNPs
+	#####Copy header and column labels in a subset file (<chrnb>.subset_<type of selction criteria>.vcf) in PATH/SelectionofSNPs
+	with gzip.open(files, "r") as fi:
+		with open(outputfileID, "w") as fo:
+			for line in fi:
+				if line.startswith("#"):
+					fo.write(line)
+				else :
+					break
+
+
 
 
  	#####Filter dataframe to have only snps corresponding to the file
-	chrnb = int(re.sub(BEFORECHRNB, '', re.sub(AFTERCHRNB, '', files)))
+
+	filteredmatchpos = matchingpositions[ matchingpositions.CHROM == chrnb ]
+	filteredmatchref = matchingreferences[ matchingreferences.CHROM == chrnb ]
+
+	#### Use awk to get directly to the line of interest (faster)
+	for line in filteredmatchpos["Corresponding row in vcf file"] :
+		os.system("zcat {0} | awk 'NR=={1} {{print;exit}}' >> {2}".format(files,line,outputfilePOS))
 
 
 	####Loop through the indexes of the filtered df
